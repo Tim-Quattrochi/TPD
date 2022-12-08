2e327e1a84936657b73173d4894069de8b2c986a
PeteyP <104457453+PeteyP801@users.noreply.github.com>
2022-12-07 22:36:27 -0600

    pushing added files and file name changes.

--- packages/server/routes/auth.routes.js
+++ packages/server/routes/auth.routes.js
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import { signUp } from '../controllers/auth.controller';
+import { Router } from "express";
+import { signUp } from "../controllers/authController";
 
 const authRouter = Router();
 
 //http://localhost:3001/api/auth/users for postman. This is
 //a simple GET. for testing. Would be a POST for sign up.
-authRouter.get('/users', signUp);
+authRouter.get("/users", signUp);
 
 export default authRouter;
