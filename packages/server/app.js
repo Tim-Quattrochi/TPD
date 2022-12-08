a3bf64397410e7ac816cf4ced0a7e3e26274223e
Tim Quattrochi <timq82@gmail.com>
2022-12-07 23:04:04 -0500

    added middleware : cors, express.json

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express, { Router } from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
+import cors from 'cors';
+import colors from 'colors';
 import { api } from './controllers/api.controller';
 
 const DB_URI = process.env.DB_URI;
@@ -14,19 +16,25 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Connected to Database!'))
+  .then(() => console.log('Connected to Database!'.cyan.underline))
   .catch((err) =>
-    console.log('An error occurred connecting to the db: ', err)
+    console.log(
+      'An error occurred connecting to the db: '.red.underline.bold,
+      err
+    )
   );
 
 const app = express();
 
 //middleware here
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //use the api from api.controller.js
 app.use(api);
 
 app.listen(PORT, () =>
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`.bgBlue)
 );
