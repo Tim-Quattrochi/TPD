e33359d554535b2ae32639954caba6a86b1f7f93
Tim Quattrochi <timq82@gmail.com>
2022-12-10 19:32:24 -0500

    refactored user model git add packages/server/models/user.js

--- packages/server/models/user.js
+++ packages/server/models/user.js
@@ -1,57 +1,65 @@
 import mongoose from 'mongoose';
-//testing git branches and issue keys with Jira.
-
-
-
-
-
-/*
-
-
-
+const { ObjectId } = mongoose.Schema.Types;
+import { MdAccountCircle } from 'react-icons/md';
 
 const userSchema = new mongoose.Schema({
-username: {
-type: String,
-unique: true,
-required: true },
-password: {
-type: String,
-required: true },
-email: {
-type: String,
-unique: true,
-required: true },
-profileImage: {
-type: String,
-default: “/someIcon.jpg” },
-posts: {
-type: ObjectID
-ref: “some endpoint” },
-commissions: {
-type: ObjectId
-ref: “someEndpoint” },
-tickets: {
-type: ObjectId
-Capstone TPD Project Plan
-ref: “someEndpoint” },
-authorizedUsers: {[
-username: {
-type: String,
-unique: true,
-required: true },
-password: {
-type: String,
-required: true },
-email: {
-type: String,
-unique: true,
-required: true },
-]},
-)
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+    default: MdAccountCircle,
+  },
+  posts: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  commissions: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  tickets: {
+    type: ObjectId,
+    ref: 'User',
+  },
+});
 
+const User = mongoose.model('User', userSchema);
 
-
-
-*/
+/* Stretch goal for sub users */
+//     authorizedUsers: {
+//         [
+// username: {
+// type: String,
+// unique: true,
+// required: true },
+// password: {
+// type: String,
+// required: true },
+// email: {
+// type: String,
+// unique: true,
+// required: true },
+// ]},
+// )
