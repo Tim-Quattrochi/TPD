5fff2600deb5cde60cf941fe68830b1e338c8787
DDaffin <daffin.destiny95@gmail.com>
2022-12-10 18:26:34 -0800

    FrontEndRoutes Active (no clicks or state yet)

---
+++ packages/client/src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import UserInfo from '../components/UserInfo'
+import UserProjects from '../components//UserProjects'
+import UserTickets from '../components/UserTickets'
+import UserMessages from '../components/UserMessages'
+
+
+export default function UserDashboard(props) {
+
+    
+return (
+
+<>
+    ~<UserInfo /> ~
+    <UserProjects /> ~
+    <UserTickets /> ~
+    <UserMessages /> ~
+    
+</>
+
+
+)}
--- packages/client/src/pages/templates.js
+++ packages/client/src/pages/templates.js
@@ -7,7 +7,23 @@ export default function HomePage(props) {
     
 return (
 
-    <div>   Templates PAGE   </div>
+    <div>   
+    
+ 
+    
+    Templates PAGE   
+    
+    <div id='templates'>   templates go  in this div </div>
+    
+    <div id='logos'> Maybe insert some logos here for use </div>
+    
+    <div id='schemes'> Maybe out some texture and/or Color schemes here for use or ideas </div>
+    
+    <div id='artists' > Maybe place promoted art sites here that  people can  go search  or commision  for things </div>
+     
+    
+    
+    </div>
 
 
 )}
