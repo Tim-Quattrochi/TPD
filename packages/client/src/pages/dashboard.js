0c9744a516c9cdc48f734242d74f338fcba61f1e
Tim Quattrochi <timq82@gmail.com>
2022-12-14 20:57:29 -0500

    compared with Destiny to make sure we both had the same version

---
+++ packages/client/src/pages/dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+import UserTickets from '../components/UserTickets'
+import UserMessages from '../components/UserMessages'
+import UserInfo from '../components/UserInfo'
+import UserProjects from '../components/UserProjects'
+
+
+export default function Dashboard (props) {
+
+    
+return (
+
+    <div>   
+    <div id='userInfoDropDown'>
+        <UserInfo />
+    </div>
+    
+    <div id='userTickets'> 
+        <UserTickets />
+    </div>
+
+    <div id='userProjects'> 
+        <UserProjects />
+    </div>
+
+    <div id='msgBoard'>  
+        <UserMessages />
+    </div>
+
+    </div>
+
+
+
+
+
+)}
