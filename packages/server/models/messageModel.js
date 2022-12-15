8b1058bc17b04b0ed92d43ddfd19233627ece712
DDaffin <daffin.destiny95@gmail.com>
2022-12-14 16:41:04 -0800

    Other models implemented to add to endpoints

---
+++ packages/server/models/messageModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+
+author: {
+    type: ObjectId,
+    ref: "" 
+},
+
+created: {
+    type: Date,
+    required: date.Now 
+},
+
+text: {
+    type: String,
+    maxlength: 500,
+    required: true 
+},
+
+comments: {
+    text: {
+        type: String,
+        required: true,
+        maxlength: 200 
+    },
+
+    author: {
+        type: ObjectId,
+        ref: 'User' 
+    },
+
+    created: {
+        type: Date,
+        default: Date.now 
+    },
+},
+
+}, {timestamps: true} )
+
+
+
+
+export default messageSchema;
---
+++ packages/server/models/taskModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const taskSchema = new mongoose.Schema({
+
+companyName: {
+    type: String,
+    required: true 
+},
+
+issue: {
+    type: String,
+    required: true 
+},
+
+description: {
+    type: String,
+    required: true 
+},
+
+submitted: {
+    type: Date,
+    required: date.Now 
+}, 
+
+})
+
+
+
+export default taskSchema;
