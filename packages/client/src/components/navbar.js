4fd50a4b3ac7eebda86227ea7cbe99bbf3552793
Tim Quattrochi <timq82@gmail.com>
2022-12-15 00:33:41 -0500

    capatalized component name

--- packages/client/src/components/navbar.js
+++ packages/client/src/components/navbar.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react';
+
+
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements';
+
 
 const NavBar = () => {
     return (
     
         <>       
         
-        <nav> TPD NavBar Here </nav>
+        <Nav>
+            <NavLink to='/'>
+                <h1>Logo</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to='/about' activeStyle>
+                    About
+                </NavLink>
+                <NavLink to='/services' activeStyle>
+                    Services
+                </NavLink>
+                <NavLink to='/contact-us' activeStyle>
+                    Contact Us
+                </NavLink>
+                <NavLink to='/sign-up' activeStyle>
+                    Sign Up
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to='/sign-in'>Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+
         
         </>
        
