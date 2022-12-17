af96549f5cebe3567b4bc7dbe23478a8fccba477
DDaffin <daffin.destiny95@gmail.com>
2022-12-17 15:56:49 -0800

    Footer Links and Base style Added

--- packages/client/src/components/Footer.js
+++ packages/client/src/components/Footer.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/temp.jpg'
 
 
 const Footer = () => {
     return (
         
-   <footer>
-        <h5> Some Footer Details here </h5>
-
-        <div>
-        <Link to='/register' > SIGN-UP  </Link> 
+   <footer className='bg-slate-800 text-red-600 flex flex-col text-center pb-4' >
+        {/* <h5 className='text-red-500 ' > Some Footer Details here </h5> */}
         
-        <Link to='/login' > LOG-IN   </Link> 
-            ~ copyright info ? ~
-        </div>
+        <img src={logo} className=' w-1/6 self-center p-5'  ></img>
+        
+        <ul className='content-evenly' >
+        <span className='p-5' > <Link to='/register' > SIGN-UP  </Link>  </span>
+        
+        <span className='p-5' > <Link to='/login' > LOG-IN   </Link>  </span>
 
-        <img src='../images/temp.jpg' ></img>
+        <li>~ copyright info ? ~</li>
+        </ul>
 
    </footer>
 
