e8b860125b16fbf6de3fa7fb71d5df2929ca7f6d
DDaffin <daffin.destiny95@gmail.com>
2022-12-21 10:48:08 -0800

    Login  setup: Bugs Present

--- packages/client/src/pages/LoginPage.js
+++ packages/client/src/pages/LoginPage.js
@@ -1,23 +1,36 @@
 import React from 'react'
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext, useRef } from 'react'
 
 import AuthContext from '../hooks/useAuth'
-
 import axios from '../hooks/axios'
-const LOGINPATH = './login'
-
 
 export default function LoginPage (props) {
     const { setAuth } = useContext(AuthContext)
-    /* handle submit from  Dave Gray  Tutorial 
-    const handleSubmit = async (e) =>{
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMessage, setErrMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [userName, password])
+
+
+    //handle submit from  Dave Gray  Tutorial 
+    const handleSignIn = async (e) =>{
         e.preventDefault() 
 
-
-
         try {
-            const response = await axios.post(LOGINPATH, 
-                    JSON.stringify(user, password), 
+            const response = await axios.post('/users/login', 
+                    JSON.stringify(userName, password), 
                 {
                     headers: { 'Content-Type' : 'application /json'},
                     withCredentials: true
@@ -25,9 +38,10 @@ export default function LoginPage (props) {
             )
             console.log(JSON.stringify(response?.data))
 
-            const accesstoken = response?.data?.accessToken
-            const roles = response?.data?.roles
-            setAuth({ user, password, roles, accessToken }) // wemay not have roles implemeneted yet and it ay  throw errors
+            const accessToken = response?.data?.accessToken
+            // const roles = response?.data?.roles
+            //Authcontroller in  controllers  to define roles minute 28:00
+            setAuth({ user, password, accessToken }) // wemay not have roles implemeneted yet and it ay  throw errors
 
             setUser('')
             setPassword('')
@@ -50,8 +64,6 @@ export default function LoginPage (props) {
             errRef.current.focus() // this is use with aria for onscreen  reading
         }
     }
-    
-    */
 
 
 
@@ -60,29 +72,46 @@ return (
 
     <div className='flex flex-col text-center p-20'>
      
-    <h2 className=' text-red-900 font-bold' > 
-        Sign in Here 
-    </h2>
-
-    <form>
-
-        <label className='flex flex-col items-center' >
-            Username:
-            <input type="text" name="userame"  className=' bg-slate-200 w-6/12 mb-3' /> 
-        </label>
-       
-
-        <label className='flex flex-col items-center' >
-            Password:
-            <input type="password" name="password" className=' bg-slate-200 w-6/12 mb-3' /> 
-        </label>
+        <h2 className=' text-red-900 font-bold' > 
+            Sign in Here 
+        </h2>
         
-        <input type="submit" value="Sign In" className=' bg-red-800 w-2/12 text-gray-200 self-center' />
+        <form onSubmit={handleSignIn} >
+            <label htmlFor='username' className='flex flex-col items-center' > </label>
+            <input  type="text" 
+                    name="userame"
+                    id='username'
+                    placeholder='Username...'
+                    ref={userRef}
+                    value={user}
+                    required
+                    autoComplete="off"
+                    onChange={(e) => setUserName(e.target.value)}
+                    className=' bg-slate-200 w-6/12 mb-3' /> 
 
 
-    </form>
+            <label htmlFor='password' className='flex flex-col items-center' > </label>
+            <input  type="password" 
+                    name="password" 
+                    id='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={pwd}
+                    required
+                    className=' bg-slate-200 w-6/12 mb-3' /> 
+
+            <button className=' bg-red-800 w-2/12 text-gray-200 self-center' > 
+                Sign-in
+            </button>
+        </form>
+
+        <p  ref={errRef} 
+            className={`${errMessage ? "visible" : "invisible"} `} 
+            aria-live="assertive" >
+                {errMessage}
+        </p>
     
     </div>
 
 
 )}
+
