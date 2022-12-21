0fedfa14a9862a9288ee1716c98bc6bd087a4c06
DDaffin <daffin.destiny95@gmail.com>
2022-12-20 20:45:01 -0800

    Fixed validatecode; Bug-axios404

--- packages/client/src/pages/RegistrationPage.js
+++ packages/client/src/pages/RegistrationPage.js
@@ -8,7 +8,6 @@ import axios from '../hooks/axios';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const regURL = '/signup'
 
 export default function RegistrationPage (props) {
     const userRef = useRef()
@@ -26,8 +25,8 @@ export default function RegistrationPage (props) {
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
 
-    const [matchPassword, setMatchPassword] = useState('')
-    const [validMatch, setValidMatvalidMatch] = useState(false)
+    const [confirmPassword, setconfirmPassword] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
     const [matchFocus, setMatchFocus] = useState(false)
 
     const [errMsg, setErrMsg] = useState('')
@@ -39,26 +38,29 @@ export default function RegistrationPage (props) {
         userRef.current.focus()
     }, [])
 
+
     useEffect( () => {
-        const result = USER_REGEX.test(userName);
-        console.log(result)
-        console.log(userName)
-        setValidName(result)
+        setValidName(USER_REGEX.test(userName))
     }, [userName]) //do we want the result on  cnsole? 
 
-    useEffect( () => {
-        const result = PASSWORD_REGEX.test(password);
-        console.log(result)
-        console.log(password)
-        setValidName(result)
 
-        const match = password === matchPassword
-        setMatchPassword(match)
-    }, [password, matchPassword])
+    useEffect( () => {
+        setValidPassword(PASSWORD_REGEX.test(password))
+
+        console.log(`${confirmPassword}: before compare`)
+        console.log(password)
+        console.log(password === confirmPassword ? true : false)
+        
+
+
+        password === confirmPassword ? setValidMatch(true) : setValidMatch(false)
+        console.log(`${confirmPassword}: after compare`)
+
+    }, [password, confirmPassword])
 
     useEffect( () => {
         setErrMsg('')
-    }, [userName, password, matchPassword, email, firstName, lastName])
+    }, [userName, password, confirmPassword, email, firstName, lastName])
 
     const handleRegistration = async (e) => {
         e.preventDefault()
@@ -70,8 +72,8 @@ export default function RegistrationPage (props) {
             return
         }
         try {
-            const response = await axios.post(regURL,
-                JSON.stringify({ userName, password, email, firstName, lastName }),
+            const response = await axios.post('/signup',
+                JSON.stringify({ firstName, lastName, email, password, userName }),
                 {
                     headers: { 'Content-Type': 'application/json' },
                     withCredentials: true
@@ -86,7 +88,7 @@ export default function RegistrationPage (props) {
             //need value attrib on inputs for this
             setUserName('');
             setPassword('');
-            setMatchPassword('');
+            setconfirmPassword('');
         } catch (err) {
             if (!err?.response) {
                 setErrMsg('No Server Response');
@@ -99,11 +101,13 @@ export default function RegistrationPage (props) {
         }
     }
 
+
+
 return (
     <>
-        <div className='flex flex-col text-center p-20' >   
-
-            <h2 className=' text-red-900 font-bold' > Begin your companies online expansion! </h2>
+        <div className='flex flex-col items-center text-center p-20 bg-cyan-900' >   
+        <div  className='bg-white w-fit pl-8 pr-8 pb-5 pt-5'>
+            <h2 className=' text-red-900 font-bold shadow-sm' > Begin your companies online expansion! </h2>
             
             <p ref={errRef} 
             aria-live="assertive" 
@@ -120,7 +124,7 @@ return (
                             name="firstName" 
                             placeholder='First Name...' 
                             required
-                            className=' bg-slate-200 w-6/12 mb-3' 
+                            className=' bg-slate-200 w-9/12 ml-4 mb-3' 
                     /> 
                 </div>
 
@@ -130,12 +134,12 @@ return (
                             name="lastName" 
                             placeholder='Last Name...' 
                             required
-                            className=' bg-slate-200 w-6/12 mb-3' 
+                            className=' bg-slate-200 w-9/12 ml-4 mb-3' 
                     /> 
                 </div>
 
                 <div className='usernameInputField w-full p-2' >
-                    <label htmlFor='username' className='p-2'>
+                    <label htmlFor='userName' className='p-2'>
                         <span className={validName ? "visible" : "invisible"}>
                             <FontAwesomeIcon icon={faCheck}  />
                         </span>
@@ -146,7 +150,7 @@ return (
                     <input  type="text"
                             placeholder='Username...'  
                             name="username" 
-                            id="username"
+                            id="userName"
                             ref={userRef}
                             autoComplete="off"
                             value={userName}
@@ -157,13 +161,14 @@ return (
                             onFocus={() => setUserFocus(true)}
                             onBlur={() => setUserFocus(false)}
                             
-                            className=' bg-slate-200 w-6/12 mb-3'
+                            className=' bg-slate-200 w-8/12 mb-3'
                     /> 
-                    <p id="uidnote" className={`${userFocus && userName && !validName ? "visible text-left pl-64" : "invisible p-0 m-0 h-0 w-0"} `}>
+                    <p id="uidnote" className={`${userFocus && userName && !validName ? "visible text-left pl-34 flex-wrap" : "invisible p-0 m-0 h-0 w-0"} `}>
                         <FontAwesomeIcon icon={faInfoCircle} />
                             4 to 24 characters.<br />
                             Must begin with a letter.<br />
-                            Letters, numbers, underscores, hyphens allowed.
+                            Letters, numbers, underscores,<br />
+                             hyphens allowed.
                     </p>
                 </div>
 
@@ -173,7 +178,7 @@ return (
                             name="email" 
                             placeholder='Email...' 
                             
-                            className='bg-slate-200 w-6/12 mb-3'
+                            className='bg-slate-200 w-9/12 ml-4 mb-3'
                     /> 
                 </div>
 
@@ -185,22 +190,24 @@ return (
                     <input onChange={(e) => setPassword(e.target.value)} 
                             type="password" 
                             name="password" 
-                            placeholder='Password...'
                             id="password"
+                            placeholder='Password...'
                             value={password}
                             required
                             aria-invalid={validPassword ? "false" : "true"}
                             aria-describedby="pwdnote"
                             onFocus={() => setPasswordFocus(true)}
                             onBlur={() => setPasswordFocus(false)}
-                            className=' bg-slate-200 w-6/12 mb-3'
+                            className=' bg-slate-200 w-8/12 mb-3'
                     /> 
 
 
-                    <p id="pwdnote" className={`${passwordFocus && !validPassword ? "visible text-left pl-64" : "invisible p-0 m-0 h-0 w-0"}`}>
+                    <p id="pwdnote" className={`${passwordFocus && !validPassword ? "visible text-left pl-30" : "invisible p-0 m-0 h-0 w-0"}`}>
                             <FontAwesomeIcon icon={faInfoCircle} />
                                 8 to 24 characters.<br />
-                                Must include uppercase and lowercase letters, a number and a special character.<br />
+                                Must include uppercase and lowercase <br />
+                                letters, a number and a special 
+                                character.<br />
                                 Allowed special characters: 
                                 <span aria-label="exclamation mark"> !</span> 
                                 <span aria-label="at symbol">@</span> 
@@ -213,16 +220,16 @@ return (
                 <div className='pswConfirmInputField w-full p-2'>
 
                     <label htmlFor="confirm_password" className='p-2' >
-                        <FontAwesomeIcon icon={faCheck} className={validMatch && matchPassword ? "visible" : "invisible"} />
-                        <FontAwesomeIcon icon={faTimes} className={!validMatch || !matchPassword ? "visible" : "invisible"} />
+                        <FontAwesomeIcon icon={faCheck} className={validMatch && confirmPassword ? "visible" : "invisible"} />
+                        <FontAwesomeIcon icon={faTimes} className={!validMatch || !confirmPassword ? "visible" : "invisible"} />
                     </label>
 
-                    <input  onChange={(e) => setMatchPassword(e.target.value)} 
+                    <input  onChange={(e) => setconfirmPassword(e.target.value)} 
                             type="password" 
                             name="confirmPassword" 
                             id="confirm_password"
                             placeholder='Confirm Password...' 
-                            // value={matchPassword}
+                            // value={confirmPassword}
                             required
                             aria-invalid={validMatch ? "false" : "true"}
                             aria-describedby="confirmnote"
@@ -230,21 +237,22 @@ return (
                             onBlur={() => setMatchFocus(false)}
 
 
-                            className=' bg-slate-200 w-6/12 mb-3'
+                            className=' bg-slate-200 w-8/12 mb-3'
                     /> 
 
-                    <p id="confirmnote" className={matchFocus && !validMatch ? "visible text-left pl-64" : "invisible p-0 m-0 h-0 w-0"}>
+                    <p id="confirmnote" className={matchFocus && !validMatch ? "visible text-left pl-30" : "invisible p-0 m-0 h-0 w-0"}>
                         <FontAwesomeIcon icon={faInfoCircle} />
                             Must match the first password input field.
                     </p>
                 </div>
                     
-                <button className=' bg-red-800 w-2/12 text-gray-200 self-center p-2'
+                <button className=' bg-red-800 w-4/12 text-gray-200 self-center p-2'
                         disabled={!validName || !validPassword || !validMatch ? true : false} > 
                     Register 
                 </button>
 
             </form>
+        </div>
         </div>
     </>
 )}
