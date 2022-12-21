16e21e24dd7e1c1ee26a36f9548f1f86e4cf01cd
DDaffin <daffin.destiny95@gmail.com>
2022-12-21 13:06:25 -0800

    Registration Successful!

--- packages/server/middleware/credentials.js
+++ packages/server/middleware/credentials.js
@@ -1,8 +1,8 @@
-const allowedOrigins = require('../config/allowedOrigins');
+const authorizedOrigins = require('../config/authorizedOrigins');
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
+    if (authorizedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
     }
     next();
