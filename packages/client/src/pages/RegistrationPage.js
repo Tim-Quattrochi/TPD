1cc37053b49341eb8f43523791c3f934a10d32e4
DDaffin <daffin.destiny95@gmail.com>
2022-12-20 21:28:58 -0800

    RegForm Styles Recovered/ 404BugAxios

--- packages/client/src/pages/RegistrationPage.js
+++ packages/client/src/pages/RegistrationPage.js
@@ -30,8 +30,8 @@ export default function RegistrationPage(props) {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [matchPassword, setMatchPassword] = useState('');
-  const [validMatch, setValidMatvalidMatch] = useState(false);
+  const [confirmPassword, setconfirmPassword] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
   const [matchFocus, setMatchFocus] = useState(false);
 
   const [errMsg, setErrMsg] = useState('');
@@ -87,7 +87,7 @@ export default function RegistrationPage(props) {
       //need value attrib on inputs for this
       setUserName('');
       setPassword('');
-      setMatchPassword('');
+      setconfirmPassword('');
     } catch (err) {
       if (!err?.response) {
         setErrMsg('No Server Response');
@@ -102,210 +102,157 @@ export default function RegistrationPage(props) {
 
   return (
     <>
-      <div className="flex flex-col text-center p-20">
-        <h2 className=" text-red-900 font-bold">
-          {' '}
-          Begin your companies online expansion!{' '}
-        </h2>
-
-        <p
-          ref={errRef}
-          aria-live="assertive"
-          className={`${errMsg ? 'errmsg' : 'offscreen'}`}
-        >
-          {' '}
-          {errMsg}{' '}
-        </p>
-
-        <form
-          onSubmit={handleRegistration}
-          className="flex flex-col items-center"
-        >
-          <div className="firstNameInputField w-full pl-9 p-2">
-            <input
-              onChange={(e) => setFirstName(e.target.value)}
-              type="text"
-              name="firstName"
-              placeholder="First Name..."
-              required
-              className=" bg-slate-200 w-6/12 mb-3"
-            />
-          </div>
-
-          <div className="lastNameInputField w-full pl-9 p-2">
-            <input
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
-              name="lastName"
-              placeholder="Last Name..."
-              required
-              className=" bg-slate-200 w-6/12 mb-3"
-            />
-          </div>
-
-          <div className="usernameInputField w-full p-2">
-            <label htmlFor="username" className="p-2">
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={validName ? 'valid' : 'hide'}
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={
-                  validName || !userName ? 'hide' : 'invalid'
-                }
-              />
-            </label>
-            <input
-              type="text"
-              placeholder="Username..."
-              name="username"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              value={userName}
-              required
-              aria-invalid={validName ? 'false' : 'true'}
-              aria-describedby="uidnote"
-              onChange={(e) => setUserName(e.target.value)}
-              onFocus={() => setUserFocus(true)}
-              onBlur={() => setUserFocus(false)}
-              htmlFor="username"
-              className=" bg-slate-200 w-6/12 mb-3"
-            />
-            <p
-              id="uidnote"
-              className={`${
-                userFocus && userName && !validName
-                  ? 'instructions'
-                  : 'offscreen'
-              } text-left pl-64`}
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              4 to 24 characters.
-              <br />
-              Must begin with a letter.
-              <br />
-              Letters, numbers, underscores, hyphens allowed.
+        <div className='flex flex-col items-center text-center p-20 bg-cyan-900' >   
+        <div  className='bg-white w-fit pl-8 pr-8 pb-5 pt-5'>
+            <h2 className=' text-red-900 font-bold shadow-sm' > Begin your companies online expansion! </h2>
+            
+            <p ref={errRef} 
+            aria-live="assertive" 
+            className={`${ errMsg ? "errmsg" : "offscreen"}`} 
+            > 
+                {errMsg} 
             </p>
-          </div>
 
-          <div className="emailInputField w-full pl-9 p-2">
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              name="email"
-              placeholder="Email..."
-              className="bg-slate-200 w-6/12 mb-3"
-            />
-          </div>
+            <form onSubmit={handleRegistration} className='flex flex-col items-center'>
 
-          <div className="passwordInputField w-full p-2">
-            <label htmlFor="password" className="p-2">
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={validPassword ? 'valid' : 'hide'}
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={
-                  validPassword || !password ? 'hide' : 'invalid'
-                }
-              />
-            </label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              name="password"
-              placeholder="Password..."
-              id="password"
-              value={password}
-              required
-              aria-invalid={validPassword ? 'false' : 'true'}
-              aria-describedby="pwdnote"
-              onFocus={() => setPasswordFocus(true)}
-              onBlur={() => setPasswordFocus(false)}
-              className=" bg-slate-200 w-6/12 mb-3"
-            />
+                <div className='firstNameInputField w-full pl-9 p-2' >
+                    <input  onChange={(e) => setFirstName(e.target.value)} 
+                            type="text" 
+                            name="firstName" 
+                            placeholder='First Name...' 
+                            required
+                            className=' bg-slate-200 w-9/12 ml-4 mb-3' 
+                    /> 
+                </div>
 
-            <p
-              id="pwdnote"
-              className={`${
-                passwordFocus && !validPassword
-                  ? 'instructions'
-                  : 'offscreen'
-              } text-left pl-64`}
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              8 to 24 characters.
-              <br />
-              Must include uppercase and lowercase letters, a number
-              and a special character.
-              <br />
-              Allowed special characters:
-              <span aria-label="exclamation mark"> !</span>
-              <span aria-label="at symbol">@</span>
-              <span aria-label="hashtag">#</span>
-              <span aria-label="dollar sign">$</span>
-              <span aria-label="percent">%</span>
-            </p>
-          </div>
+                <div className='lastNameInputField w-full pl-9 p-2' >
+                    <input  onChange={(e) => setLastName(e.target.value)} 
+                            type="text" 
+                            name="lastName" 
+                            placeholder='Last Name...' 
+                            required
+                            className=' bg-slate-200 w-9/12 ml-4 mb-3' 
+                    /> 
+                </div>
 
-          <div className="pswConfirmInputField w-full p-2">
-            <label htmlFor="confirm_password" className="p-2">
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={
-                  validMatch && matchPassword ? 'valid' : 'hide'
-                }
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={
-                  validMatch || !matchPassword ? 'hide' : 'invalid'
-                }
-              />
-            </label>
+                <div className='usernameInputField w-full p-2' >
+                    <label htmlFor='userName' className='p-2'>
+                        <span className={validName ? "visible" : "invisible"}>
+                            <FontAwesomeIcon icon={faCheck}  />
+                        </span>
+                        <span className={validName ? "invisible" : "visible"}>
+                            <FontAwesomeIcon icon={faTimes}  />
+                        </span>
+                    </label>
+                    <input  type="text"
+                            placeholder='Username...'  
+                            name="username" 
+                            id="userName"
+                            ref={userRef}
+                            autoComplete="off"
+                            value={userName}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onChange={(e) => setUserName(e.target.value)}
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                            
+                            className=' bg-slate-200 w-8/12 mb-3'
+                    /> 
+                    <p id="uidnote" className={`${userFocus && userName && !validName ? "visible text-left pl-34 flex-wrap" : "invisible p-0 m-0 h-0 w-0"} `}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, numbers, underscores,<br />
+                             hyphens allowed.
+                    </p>
+                </div>
 
-            <input
-              onChange={(e) => setMatchPassword(e.target.value)}
-              type="password"
-              name="confirmPassword"
-              id="confirm_password"
-              placeholder="Confirm Password..."
-              // value={matchPassword}
-              required
-              aria-invalid={validMatch ? 'false' : 'true'}
-              aria-describedby="confirmnote"
-              onFocus={() => setMatchFocus(true)}
-              onBlur={() => setMatchFocus(false)}
-              className=" bg-slate-200 w-6/12 mb-3"
-            />
+                <div className='emailInputField w-full pl-9 p-2' > 
+                    <input  onChange={ (e) => setEmail(e.target.value)} 
+                            type="email" 
+                            name="email" 
+                            placeholder='Email...' 
+                            
+                            className='bg-slate-200 w-9/12 ml-4 mb-3'
+                    /> 
+                </div>
 
-            <p
-              id="confirmnote"
-              className={`{matchFocus && !validMatch ? "instructions" : "offscreen"} text-left pl-64`}
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              Must match the first password input field.
-            </p>
-          </div>
+                <div className='passwordInputField w-full p-2'> 
+                    <label htmlFor='password' className='p-2' >
+                            <FontAwesomeIcon icon={faCheck} className={validPassword ? "visible" : "invisible"} />
+                            <FontAwesomeIcon icon={faTimes} className={validPassword ? "invisible" : "visible"} />     
+                    </label>
+                    <input onChange={(e) => setPassword(e.target.value)} 
+                            type="password" 
+                            name="password" 
+                            id="password"
+                            placeholder='Password...'
+                            value={password}
+                            required
+                            aria-invalid={validPassword ? "false" : "true"}
+                            aria-describedby="pwdnote"
+                            onFocus={() => setPasswordFocus(true)}
+                            onBlur={() => setPasswordFocus(false)}
+                            className=' bg-slate-200 w-8/12 mb-3'
+                    /> 
 
-          <button
-            className=" bg-red-800 w-2/12 text-gray-200 self-center p-2"
-            disabled={
-              !validName || !validPassword || !validMatch
-                ? true
-                : false
-            }
-          >
-            Register
-          </button>
 
-        </form>
-      </div>
+                    <p id="pwdnote" className={`${passwordFocus && !validPassword ? "visible text-left pl-30" : "invisible p-0 m-0 h-0 w-0"}`}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                                8 to 24 characters.<br />
+                                Must include uppercase and lowercase <br />
+                                letters, a number and a special 
+                                character.<br />
+                                Allowed special characters: 
+                                <span aria-label="exclamation mark"> !</span> 
+                                <span aria-label="at symbol">@</span> 
+                                <span aria-label="hashtag">#</span> 
+                                <span aria-label="dollar sign">$</span> 
+                                <span aria-label="percent">%</span>
+                    </p>
+                </div>
+    
+                <div className='pswConfirmInputField w-full p-2'>
+
+                    <label htmlFor="confirm_password" className='p-2' >
+                        <FontAwesomeIcon icon={faCheck} className={validMatch && confirmPassword ? "visible" : "invisible"} />
+                        <FontAwesomeIcon icon={faTimes} className={!validMatch || !confirmPassword ? "visible" : "invisible"} />
+                    </label>
+
+                    <input  onChange={(e) => setconfirmPassword(e.target.value)} 
+                            type="password" 
+                            name="confirmPassword" 
+                            id="confirm_password"
+                            placeholder='Confirm Password...' 
+                            // value={confirmPassword}
+                            required
+                            aria-invalid={validMatch ? "false" : "true"}
+                            aria-describedby="confirmnote"
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+
+
+                            className=' bg-slate-200 w-8/12 mb-3'
+                    /> 
+
+                    <p id="confirmnote" className={matchFocus && !validMatch ? "visible text-left pl-30" : "invisible p-0 m-0 h-0 w-0"}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                            Must match the first password input field.
+                    </p>
+                </div>
+                    
+                <button className=' bg-red-800 w-4/12 text-gray-200 self-center p-2'
+                        disabled={!validName || !validPassword || !validMatch ? true : false} > 
+                    Register 
+                </button>
+
+            </form>
+        </div>
+        </div>
     </>
-  );
+)
 }
 
 
