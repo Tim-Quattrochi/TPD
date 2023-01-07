c8e2fadb7dbe50d5dcee21971e5e595b425722aa
DDaffin <daffin.destiny95@gmail.com>
2023-01-07 00:32:43 -0800

    Designs Page Start

--- packages/client/src/pages/TemplatesPage.js
+++ packages/client/src/pages/TemplatesPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import designSchemes from '../images/designObj'
 
 
 export default function TemplatesPage (props) {
@@ -7,19 +7,36 @@ export default function TemplatesPage (props) {
     
 return (
 
-    <div>   
+<>
+<h2 className='text-red-900 font-semibold pt-10 pb-10 text-3xl text-center' > 
+    Inspration for Your Dream
+    </h2>
     
-    Templates PAGE   
+    <div className="pl-44 h-screen flex flex-col">  
+
+ 
     
-    <div id='templates'>   templates go  in this div </div>
+    <div id='templates' className=' h-11/12 max-w-fit overflow-auto mr-20 ml-20'> 
+
+            {designSchemes.templates.map( item =>  <img src={item.picture} className="pl-20 pb-20 pr-20 h-96 w-96" ></img>  )}
+        
+    </div>
     
-    <div id='logos'> Maybe insert some logos here for use </div>
+    <div id='logos' className=' h-11/12 max-w-fit overflow-auto  mr-20 ml-20'> 
+
+            {designSchemes.logos.map( item =>  <img src={item.picture} className=" pl-20 pb-20 pr-20 h-96 w-96" ></img>  )}
     
-    <div id='schemes'> Maybe out some texture and/or Color schemes here for use or ideas </div>
+    </div>
     
-    <div id='artists' > Maybe place promoted art sites here that  people can  go search  or commision  for things </div>
+    {/* <div id='schemes' className=' h-11/12 max-w-fit overflow-auto  mr-20 ml-20'> 
+
+            {designSchemes.colorTemplates.map( item => <img src={item.picture} className="pl-20 pb-20 pr-20 h-60 w-auto" ></img>)}
+      
+    </div> */}
+    
+    
      
     </div>
-
+</>
 
 )}
