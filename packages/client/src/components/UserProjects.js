e3dee7db316654bf109ae6128677e316e38a8396
PeteyP <104457453+PeteyP801@users.noreply.github.com>
2023-01-07 01:25:12 -0600

    added some design elements
    Co-authored-by: Tim Quattrochi <Tim-Quattrochi@users.noreply.github.com>

--- packages/client/src/components/UserProjects.js
+++ packages/client/src/components/UserProjects.js
@@ -4,86 +4,79 @@ import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 const UserProjects = () => {
-  const axios = useAxiosPrivate();
+	const axios = useAxiosPrivate();
 
-  const auth = useAuth();
+	const auth = useAuth();
 
-  const [userId] = useState(null);
+	const [userId] = useState(null);
 
-  const [projects, setProjects] = useState([]);
+	const [projects, setProjects] = useState([]);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  //gets the users project by their req.id so technically null can be passed and it
-  //will still get this particular user projects based on their user id from the req header.
-  useEffect(() => {
-    const getProjects = async () => {
-      const res = await axios
-        .get(`/project/user/${userId}`)
-        .then((res) => setProjects(res.data.data.projects));
-    };
+	//gets the users project by their req.id so technically null can be passed and it
+	//will still get this particular user projects based on their user id from the req header.
+	useEffect(() => {
+		const getProjects = async () => {
+			const res = await axios
+				.get(`/project/user/${userId}`)
+				.then((res) => setProjects(res.data.data.projects));
+		};
 
-    getProjects();
-  }, [axios, userId]);
+		getProjects();
+	}, [axios, userId]);
 
-  const handleEdit = (id) => {
-    navigate(`/projects/edit/${id}`);
-  };
+	const handleEdit = (id) => {
+		navigate(`/projects/edit/${id}`);
+	};
 
-  const handleDelete = async (id) => {
-    try {
-      const deleteProject = await axios.delete(`/project/${id}`);
-      console.log(deleteProject);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const handleDelete = async (id) => {
+		try {
+			const deleteProject = await axios.delete(`/project/${id}`);
+			console.log(deleteProject);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className="bg-gray-200 p-4">
-      {projects &&
-        projects.map((project) => (
-          <div
-            key={project._id}
-            className="bg-white rounded shadow p-4 mb-4"
-          >
-            <h3 className="text-xl font-semibold text-slate-800">
-              Project Name: {project.projectName}
-            </h3>
-            <ul className="list-disc pl-4 font-light  text-sky-900 leading-loose">
-              Project Details:{" "}
-              {project.projectDetails
-                .split("\n")
-                .map((detail, index) => (
-                  <li key={index}>{detail}</li>
-                ))}
-            </ul>
-            <p className=" text-pink-800">
-              Mission statement: {project.missionStatement}
-            </p>
-            <p className=" text-pink-800">
-              Company Name: {project.companyName}
-            </p>
-            <p className=" text-amber-800">
-              Deadline: {project.deadlines}
-            </p>
-            <button
-              className="bg-pink-800 rounded-full px-4 py-2 mx-2 text-white"
-              onClick={() => handleEdit(project._id)}
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              className="bg-red-500 rounded-full px-4 py-2 mx-2 text-white"
-              onClick={() => handleDelete(project._id)}
-            >
-              Delete
-            </button>
-          </div>
-        ))}
-    </div>
-  );
+	return (
+		<div className="bg-gray-200 p-4">
+			{projects &&
+				projects.map((project) => (
+					<div key={project._id} className="bg-white rounded shadow p-4 mb-4">
+						<h3 className="text-xl font-semibold text-slate-800">
+							Project Name: {project.projectName}
+						</h3>
+						<ul className="list-disc pl-4 font-light  text-sky-900 leading-loose">
+							Project Details:{" "}
+							{project.projectDetails.split("\n").map((detail, index) => (
+								<li key={index}>{detail}</li>
+							))}
+						</ul>
+						<p className=" text-pink-800">
+							Mission statement: {project.missionStatement}
+						</p>
+						<p className=" text-pink-800">
+							Company Name: {project.companyName}
+						</p>
+						<p className=" text-amber-800">Deadline: {project.deadlines}</p>
+						<button
+							className="bg-pink-800 rounded-full px-4 py-2 mx-2 text-white"
+							onClick={() => handleEdit(project._id)}
+						>
+							Edit
+						</button>
+						<button
+							type="button"
+							className="bg-red-500 rounded-full px-4 py-2 mx-2 text-white"
+							onClick={() => handleDelete(project._id)}
+						>
+							Delete
+						</button>
+					</div>
+				))}
+		</div>
+	);
 };
 
 export default UserProjects;
--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -1,36 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from "../images/handsWorking.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/handsWorking.jpg";
 
+export default function HomePage(props) {
+	return (
+		<>
+			<div className="pl-44  h-screen max-h-screen flex flex-col ">
+				<h1 className="text-sky-500 font-semibold p-10 pb-16 text-3xl text-center">
+					We design with your future in mind!
+				</h1>
 
-export default function HomePage (props) {
+				<div className="flex justify-between pb-20">
+					<img
+						src={logo}
+						className="w-6/12 rounded-2xl ml-5 align place-content-center"
+					/>
 
+					<button className=" mr-56">
+						<Link to="/register"> SIGN-UP </Link>{" "}
+					</button>
+				</div>
 
-
-    
-return (
-    <>
-    <div className='pl-44  h-screen max-h-screen flex flex-col '>  
-     
-        <h1 className='text-red-900 font-semibold p-10 pb-16 text-3xl text-center' > 
-        We design with your future in mind!
-       </h1> 
-         
-        
-        <div className='flex justify-between pb-20'>
-        <img src={logo} className="w-6/12 rounded-2xl ml-5 align place-content-center"/>
-
-      
-        <button className=' mr-56'><Link to='/register' > SIGN-UP  </Link> </button> 
-        </div>
-
-           
-
-        <div className=' text-center ml-20 mr-20'> 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi etiam dignissim diam quis enim lobortis. Id ornare arcu odio ut sem. Leo vel orci porta non pulvinar neque laoreet. Nisi scelerisque eu ultrices vitae. Massa vitae tortor condimentum lacinia. Nunc congue nisi vitae suscipit tellus mauris a diam. Vel quam elementum pulvinar etiam non. Urna duis convallis convallis tellus id interdum. Dapibus ultrices in iaculis nunc sed augue.
-        </div>
-        
-       
-    </div>
-    </>
-)}
+				<div class="p-4 rounded-lg text-slate-800">
+					<h3>Why Choose Total Product Design?</h3>
+					<p>
+						Total Product Design (TPD) is a growing design agency that
+						specialized in creating powerful, effective brands and web presences
+						for businesses of all sizes.
+					</p>
+					<p>
+						Our team of experienced designers takes the time to understand your
+						business, your goals, and your target audience, and we use this
+						information to create a unique, tailored design that speaks directly
+						to your audience.
+					</p>
+					<p>
+						In addition to our attention to detail and personalized approach,
+						TPD is also known for our innovative and cutting-edge designs. We
+						are always on the lookout for new trends and techniques, and we are
+						not afraid to push the boundaries in order to create something truly
+						special and memorable.
+					</p>
+					<p>
+						Overall, if you want a brand and web presence that truly stands out
+						and gets results, TPD is the perfect choice. Contact us today to
+						learn more about what we can do for your business.
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
