abcf50a99a334535420053d0a82e592f232222d5
Tim Quattrochi <timq82@gmail.com>
2023-01-10 23:21:31 -0500

    deployment tweak

--- packages/server/config/constants.js
+++ packages/server/config/constants.js
@@ -1,8 +1,10 @@
 export const PORT = process.env.PORT;
-export const DB_URI = process.env.DB_URI;
+export const DB_URI =
+  process.env.DB_URI || "mongodb://localhost:27017/TPD";
 export const API_URL = process.env.API_URL;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 export const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const NODE_ENV = process.env.NODE_ENV;
+export const DB_NAME = process.env.DB_NAME;
