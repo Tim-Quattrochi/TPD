50ae64e5b4b7a1d1e85b8b8665e478bd8da2a62f
Tim Quattrochi <timq82@gmail.com>
2023-01-12 00:27:05 -0500

    Deployment tweak for api url

--- packages/client/src/hooks/useAxios.js
+++ packages/client/src/hooks/useAxios.js
@@ -3,11 +3,11 @@ import { API_URL } from "../configs/constants";
 console.log(API_URL);
 
 export default axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL || "api/v1/",
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL || "api/v1/",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
